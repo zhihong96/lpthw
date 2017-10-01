a2c97dda0050f87f821885fcd47beb739df3e46d
zhihong96 <xiezhihong3@gmail.com>
2017-10-01 22:32:09 +0800

    add comments to each lines

---
+++ ex3.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# count chicken
+print "I will now count my chickens:"
+
+# Print the number of hens
+print "Hens",25.0+30.0/6.0
+# Print the number of rooster
+print "Roosters",100.0-25.0*3.0%4.0
+
+# Print "Now I will count the eggs:"
+print "Now I will count the eggs:"
+
+# count math
+print 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
+
+# print "Is it ture that 3+2<5-7?"
+print "Is it true that 3+2<5-7?"
+
+# test if 3+2<5-7(Ture or False)
+print 3.0+2.0<5.0-7.0
+
+# print 3+2 count results
+print "What is 3+2?",3.0+2.0
+# print 5-7 count results
+print "What is 5-7?",5.0-7.0
+
+# Print "Oh,that's why it's False."
+print "Oh,that's why it's False."
+
+# print "How about some more"
+print "How about some more."
+
+# test if 5 is greater than -2(true or flase)
+print "Is it greater?",5.0>-2.0
+# test if 5 is greater than or equal to -2(true or flase)
+print "Is it greater or equal?",5.0>=-2.0
+# test is 5 is less than or equal to -2(true or flase)
+print "Is it less or equal?",5.0<=-2.0
