7d705e3d81756c1f217f5e3199ac9a6459a99c74
zhihong96 <xiezhihong3@gmail.com>
2017-10-01 23:02:03 +0800

    extra credit

---
+++ ex5.1.py
@@ -0,0 +1,17 @@
+inches = 19
+centimetres = inches * 2.54
+print "%r inches equals %r centimetres." % (inches, centimetres)
+
+pounds = 160
+kilos = pounds /2.2
+print "%r pounds equals %r kilos." % (pounds, kilos)
+
+
+pounds = 160
+kilos = pounds * 0.45359237
+print "%r pounds equals %r kilos." % (pounds, kilos)
+
+
+x = 10
+y = x * 2
+print "%r x equals %r y." % (x, y)
